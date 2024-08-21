2f37f7b2c7fa35c098389339f3af0297110ad8cb
Rohit Jadhav <rohit.jadhav0403@gmail.com>
2024-08-21 11:24:24 +0530

    Form validation server side Golang

--- controllers/userController.go
+++ controllers/userController.go
@@ -38,14 +38,15 @@ func Registerdata(c *gin.Context) {
 
 func Logindata(c *gin.Context) {
 	var input struct {
-		Email    string `form:"email"`
-		Password string `form:"password"`
+		Email    string `form:"email" binding:"required"`
+		Password string `form:"password" binding:"required"`
 	}
 
 	if err := c.ShouldBind(&input); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Faild to bind input"})
 		return
 	}
+
 	var user models.User
 	if err := models.DB.Where("email = ?", input.Email).First(&user).Error; err != nil {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid user id and password"})
@@ -65,7 +66,7 @@ func Logindata(c *gin.Context) {
 	}
 
 	// c.JSON(http.StatusOK, gin.H{"message": "login successfuly"})
-	c.Redirect(http.StatusFound, "user/products")
+	c.JSON(http.StatusOK, gin.H{"message": "login successful", "redirect_url": "user/products"})
 }
 
 func LogoutUser(c *gin.Context) {
